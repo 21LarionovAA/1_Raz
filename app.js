09b1495cd9cbe3e51b190d3ab7d082af3705178c
21LarionovAA <larionovandrey21@vk.com>
2023-04-11 15:43:46 +0400

    newcodtest

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
-let a = 1;
-console.log(a)
-a = 'test';
-console.log(a)
+// Базовые арифметические операторы
+const width = 10;
+const height = 5;
+const space = width + height;
+const newWidht = width - 4;
+const newWidht2 = + 4;
+const division = newWidht / newWidht2;
+const volume = 2 ** 3; // 2 * 2 * 2
+console.log(volume);
+
+// строки
+const city = 'Москва';
+const street = 'Новослободская';
+console.log(city + ', ' + street + ' ' + 5);
