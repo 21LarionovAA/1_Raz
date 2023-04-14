ac368b9be413bd73e369a62f66219efa4595313e
21LarionovAA <larionovandrey21@vk.com>
2023-04-14 12:58:58 +0400

    NewApp2

--- app.js
+++ app.js
@@ -1,14 +1,5 @@
-// Базовые арифметические операторы
-const width = 10;
-const height = 5;
-const space = width + height;
-const newWidht = width - 4;
-const newWidht2 = + 4;
-const division = newWidht / newWidht2;
-const volume = 2 ** 3; // 2 * 2 * 2
-console.log(volume);
+const a = '10' - '1'
+console.log(Number('10' - '1'));
+console.log(typeof a);
 
-// строки
-const city = 'Москва';
-const street = 'Новослободская';
-console.log(city + ', ' + street + ' ' + 5);
+
