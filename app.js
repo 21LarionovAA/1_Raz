b204f1330708b417e92b4b2f3ec91c8551183b26
21LarionovAA <larionovandrey21@vk.com>
2023-04-14 22:22:34 +0400

    homeTest2

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
-const a = '10' - '1'
-console.log(Number('10' - '1'));
-console.log(typeof a);
+const money2 = 12000;
+const precent = (0.07 / 12);
+const savings = Math.floor(money2 * (1 + precent) ** 24);
+const housePrice = 13500;
+console.log(savings + '$');
 
-
+if (savings > housePrice) {
+    console.log('Можем купить дом');
+    console.log(`Остаток после покупки ${savings - housePrice}$`);
+} else {
+    console.log('Не хватает на дом');
+    console.log(`Сколько еще не хватает для покупки ${housePrice - savings}$`)
+}
